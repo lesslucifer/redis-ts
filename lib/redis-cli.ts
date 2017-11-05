ff965a62b4b368e3635c4c426b2e9dee46192502
Salm <quangvu.luu92@gmail.com>
2017-11-05 22:41:32 +0700

    + hash impl

--- lib/redis-cli.ts
+++ lib/redis-cli.ts
@@ -12,6 +12,11 @@ interface RedisParser<F, T> {
     (v: F): Promise<T>
 }
 
+interface RedisScanResult {
+    cursor: number;
+    values: string[];
+}
+
 interface RedisKey {
     readonly key: string;
     child(path: string, sep?: string): RedisKeyAny;
@@ -38,9 +43,29 @@ interface RedisKeySet extends RedisKey {
     srandmember(count?: number): Promise<string[]>;
     srandOneMember(): Promise<string>;
     srem(...members: RedisPrimitives[]): Promise<number>;
-    sscan(cursor: string, pattern?: string, count?: number): Promise<string[]>;
+    sscan(cursor: string, pattern?: string, count?: number): Promise<RedisScanResult>;
     sunion(...keys: (RedisKey | string)[]): Promise<Set<string>>;
     sunionstore(...keys: (RedisKey | string)[]): Promise<number>;
+}
+
+interface RedisKeyHash extends RedisKey {
+    hdel(...fields: string[]): Promise<number>;
+    hexist(field: string): Promise<boolean>;
+    hget(field: string): Promise<string>;
+    hgetall(): Promise<{[field: string]: string}>
+    hincrby(field: string, by: number): Promise<number>;
+    hincrbyfloat(field: string, by: number): Promise<number>;
+    hkeys(): Promise<string[]>;
+    hlen(): Promise<number>;
+    hmget(...fields: string[]): Promise<string[]>;
+    hmgetDict(...fields: string[]): Promise<{[key: string]: string}>
+    hmset(...data: RedisPrimitives[]): Promise<boolean>;
+    hmsetDict(data: {[field: string]: RedisPrimitives}): Promise<boolean>;
+    hset(field: string, value: RedisPrimitives): Promise<boolean>;
+    hsetnx(field: string, value: RedisPrimitives): Promise<boolean>;
+    hstrlen(field: string): Promise<number>;
+    hvals(): Promise<string[]>;
+    hscan(cursor: string, pattern?: string, count?: number): Promise<RedisScanResult>;
 }
 
 interface RedisKeyAny extends RedisKeyString, RedisKeySet {
@@ -69,6 +94,7 @@ export class RedisClient implements RedisKeyAny {
         return this.bbClient.delAsync(this.key);
     }
 
+    // #region Strings
     get(): Promise<string> {
         return this.bbClient.getAsync(this.key);
     }
@@ -76,7 +102,9 @@ export class RedisClient implements RedisKeyAny {
     set(value: RedisPrimitives): Promise<void> {
         return this.bbClient.setAsync(this.key, value);
     }
+    // #endregion
 
+    // #region Sets
     sadd(...values: RedisPrimitives[]): Promise<number> {
         return this.bbClient.saddAsync(this.key, ...values);
     }
@@ -133,19 +161,117 @@ export class RedisClient implements RedisKeyAny {
         return this.bbClient.sremAsync(this.key, ...members);
     }
 
-    sscan(cursor: string, pattern?: string, count?: number): Promise<string[]> {
+    sscan(cursor: string, pattern?: string, count?: number): Promise<RedisScanResult> {
         const args: any[] = [cursor];
         pattern !== undefined && args.push('MATCH'), args.push(pattern as string);
         count !== undefined && args.push('COUNT'), args.push(count as number);
         
-        return this.bbClient.sscanAsync(this.key, ...args);
+        return this.bbClient.sscanAsync(this.key, ...args).then((resp: any) => {
+            return {
+                cursor: utils.parseIntNull(resp[0]),
+                valuse: resp[1]
+            }
+        });
     }
 
     sunion(...keys: (RedisKey | string)[]): Promise<Set<string>> {
-        return this.bbClient.sunionAsync(this.key, ...this.mkKeys(keys)).then((mems: string) => new Set(mems));
+        return this.bbClient.sunionAsync(this.key, ...this.mkKeys(keys)).then((mems: string[]) => new Set(mems));
     }
 
     sunionstore(...keys: (RedisKey | string)[]): Promise<number> {
         return this.bbClient.sunionstoreAsync(this.key, ...this.mkKeys(keys));
     }
+    // #endregion
+
+    // #region Hashes
+    hdel(...fields: string[]): Promise<number> {
+        return this.bbClient.hdelAsync(this.key, ...fields);
+    }
+
+    hexist(field: string): Promise<boolean> {
+        return this.bbClient.hexistAsync(this.key, field).then(utils.asBoolean);
+    }
+
+    hget(field: string): Promise<string> {
+        return this.bbClient.hgetAsync(this.key, field);
+    }
+    
+    hgetall(): Promise<{[field: string]: string}> {
+        return this.bbClient.hgetallAsync(this.key).then(utils.asDictionary);
+    }
+            
+    hincrby(field: string, by: number): Promise<number> {
+        return this.bbClient.hincrbyAsync(this.key, field, by);
+    }
+
+    hincrbyfloat(field: string, by: number): Promise<number> {
+        return this.bbClient.hincrbyfloatAsync(this.key, field, by);
+    }
+    
+    hkeys(): Promise<string[]> {
+        return this.bbClient.hkeysAsync(this.key);
+    }
+
+    hlen(): Promise<number> {
+        return this.bbClient.hlenAsync(this.key);
+    }
+
+    hmget(...fields: string[]): Promise<string[]> {
+        return this.bbClient.hmgetAsync(this.key, ...fields);
+    }
+    
+    hmgetDict(...fields: string[]): Promise<{[key: string]: string}> {
+        return this.hmget(...fields).then(vals => {
+            const dict: {[key: string]: string} = {};
+            for (let i = 0; i < fields.length; ++i) {
+                dict[fields[i]] = vals[i];
+            }
+            return dict;
+        })
+    }
+
+    hmset(...data: RedisPrimitives[]): Promise<boolean> {
+        return this.bbClient.hmsetAsync(this.key, ...data).then(utils.asBoolean);
+    }
+    
+    hmsetDict(data: {[field: string]: RedisPrimitives}): Promise<boolean> {
+        const args: any[] = [];
+        for (const field in data) {
+            args.push(field);
+            args.push(data[field]);
+        }
+
+        return this.hmset(...args);
+    }
+    
+    hset(field: string, value: RedisPrimitives): Promise<boolean> {
+        return this.bbClient.hsetAsync(this.key, field, value).then(utils.asBoolean);
+    }
+    
+    hsetnx(field: string, value: RedisPrimitives): Promise<boolean> {
+        return this.bbClient.hsetnxAsync(this.key, field, value).then(utils.asBoolean);
+    }
+    
+    hstrlen(field: string): Promise<number> {
+        return this.bbClient.hstrlenAsync(this.key, field);
+    }
+    
+    hvals(): Promise<string[]> {
+        return this.bbClient.hvalsAsync(this.key);
+    }
+    
+    hscan(cursor: string, pattern?: string, count?: number): Promise<RedisScanResult> {
+        const args: any[] = [cursor];
+        pattern !== undefined && args.push('MATCH'), args.push(pattern as string);
+        count !== undefined && args.push('COUNT'), args.push(count as number);
+        
+        return this.bbClient.hscanAsync(this.key, ...args).then((resp: any) => {
+            return {
+                cursor: utils.parseIntNull(resp[0]),
+                valuse: resp[1]
+            }
+        });
+    }
+    
+    // #endregion
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -25,8 +25,38 @@ export function asBoolean(val: any) {
     return false;
 }
 
+function isObject(value: any) {
+    const type = typeof value
+    return value != null && (type == 'object' || type == 'function')
+}
+
+export function asDictionary(val: any): {[key: string]: string} | undefined {
+    if (isObject(val)) {
+        return val;
+    }
+
+    if (Array.isArray(val) && val.length % 2 == 0) {
+        const dict: {[key: string]: string} = {};
+        const n = val.length / 2;
+        for (let i = 0; i < n; ++i) {
+            dict[<string> val[2 * n]] = val[2 * n + 1];
+        }
+    }
+
+    return undefined;
+}
+
 export function first(array: any[]) {
   return (array != null && array.length)
     ? array[0]
     : undefined
+}
+
+export function parseIntNull(val: any) {
+    const i = parseInt(val);
+    if (!isNaN(i)) {
+        return i;
+    }
+
+    return null;
 }
